420ad120469d5f3b1fa0aad170be6a7f59dd88cd
mert <mertaktas231@gmail.com>
2019-10-20 23:22:46 +0300

    Passport yapısı kuruldu.

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/users"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
 
+mongoose.connect("mongodb://localhost:27017/auth_demo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "Hello everbody lets go!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -13,5 +38,4 @@ app.get("/secret", function (req, res) {
 
 app.listen(3000, function () {
     console.log("Server Started....");
-    ""
 });
