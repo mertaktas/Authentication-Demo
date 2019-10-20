8ccb1bbb6a0470f9624d9528e78f55fb940d5a46
mert <mertaktas231@gmail.com>
2019-10-20 23:51:27 +0300

    Yeni üyelik oluşturma.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+// VARIABLES
 var express = require("express"),
     mongoose = require("mongoose"),
     passport = require("passport"),
     bodyParser = require("body-parser"),
     User = require("./models/users"),
     LocalStrategy = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose")
+    passportLocalMongoose = require("passport-local-mongoose"),
+    app = express();
 
+
+// APP CONFİG
 mongoose.connect("mongodb://localhost:27017/auth_demo", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
-var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(require("express-session")({
     secret: "Hello everbody lets go!",
     resave: false,
@@ -28,6 +32,10 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
+// =====================
+// ROUTES
+// =====================
+
 app.get("/", function (req, res) {
     res.render("home");
 });
@@ -35,6 +43,30 @@ app.get("/", function (req, res) {
 app.get("/secret", function (req, res) {
     res.render("secret");
 });
+
+// AUTH ROUTES
+// show sign up form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register", function (req, res) {
+    req.body.username
+    req.body.password
+    User.register(new User({
+        username: req.body.username
+    }), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/secret");
+        });
+    });
+});
+
 
 app.listen(3000, function () {
     console.log("Server Started....");
